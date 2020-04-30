cb16ce6eba73c79d1dcb318f3afbff6c0f8da92b
EvanBagis <43722228+EvanBagis@users.noreply.github.com>
2020-04-30 23:31:05 +0300

    Update evolution.py

--- gb_rf_evolution/evolution.py
+++ gb_rf_evolution/evolution.py
@@ -35,7 +35,8 @@ class gb_rf_evolution:
 
         models = []
         for i in range(self._generations - 1):
-            self._train_networks(x_train, y_train, x_test, y_test)
+            trained = self._train_networks(x_train, y_train, x_test, y_test)
+            models.extend(trained)
             self._networks = optimizer.evolve(self._networks)
 
         self._networks = sorted(self._networks, key=lambda x: x.accuracy, reverse=True)
@@ -53,13 +54,14 @@ class gb_rf_evolution:
         :return: None
         """
         
+        models = []
         pbar = tqdm(total=len(self._networks))
         for network in self._networks:
             model = network.train(x_train, y_train, x_test, y_test)
             pbar.update(1)
             models.append(model)
         pbar.close()
-        
+        return models
 
     def _get_average_accuracy(self, networks):
         """
