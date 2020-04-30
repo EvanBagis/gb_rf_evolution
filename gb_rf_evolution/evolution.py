ea55ca88dfaf3f2fe714d0fff6f7ec76e61fe6eb
EvanBagis <43722228+EvanBagis@users.noreply.github.com>
2020-04-30 20:31:39 +0300

    Update evolution.py

--- gb_rf_evolution/evolution.py
+++ gb_rf_evolution/evolution.py
@@ -8,7 +8,7 @@ from logger import logger
 from optimizer import Optimizer
 
 
-class NeuroEvolution:
+class gb_rf_evolution:
     def __init__(self, generations, population, params):
         """
         :param generations int: number of generation
